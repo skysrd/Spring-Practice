272ffe1de0057b2cf534bd344018f8c26e2a33f1
장민석 <skysrd@jangminseog-ui-Macmini.local>
2022-01-07 17:48:46 +0900

    [ADD] ServiceTest DI

--- src/test/java/Skysrd/SpringPractice/service/MemberServiceTest.java
+++ src/test/java/Skysrd/SpringPractice/service/MemberServiceTest.java
@@ -4,6 +4,7 @@ import Skysrd.SpringPractice.domain.Member;
 import Skysrd.SpringPractice.repository.MemoryMemberRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
@@ -12,8 +13,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MemberServiceTest {
 
-    MemberService memberService = new MemberService();
-    MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+
+    @BeforeEach
+    public void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
 
     @AfterEach
     public void afterEach() {
