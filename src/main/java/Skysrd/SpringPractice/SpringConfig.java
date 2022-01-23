f89cd37189bdc12ed1a28c19792cbf4411edfae5
skysrd <37787079+skysrd@users.noreply.github.com>
2022-01-24 08:36:32 +0900

    [ADD]Members Controller

--- src/main/java/Skysrd/SpringPractice/SpringConfig.java
+++ src/main/java/Skysrd/SpringPractice/SpringConfig.java
@@ -3,20 +3,19 @@ package Skysrd.SpringPractice;
 import Skysrd.SpringPractice.repository.MemberRepository;
 import Skysrd.SpringPractice.repository.MemoryMemberRepository;
 import Skysrd.SpringPractice.service.MemberService;
-<<<<<<< HEAD
-=======
+
 import org.springframework.beans.factory.annotation.Configurable;
->>>>>>> e03a0b7a5a45bd2646ec207c6b1b2638c601e3c7
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
 
-    @Bean
-    public MemberService memberService() {
-        return new MemberService(memberRepository());
-    }
+//    @Bean
+//    public MemberService memberService() {
+//        return new MemberService(memberRepository());
+//    }
 
     @Bean
     public MemberRepository memberRepository() {
--- src/main/java/Skysrd/SpringPractice/controller/MemberController.java
+++ src/main/java/Skysrd/SpringPractice/controller/MemberController.java
@@ -1,14 +1,17 @@
 package Skysrd.SpringPractice.controller;
 
 
+import Skysrd.SpringPractice.domain.Member;
 import Skysrd.SpringPractice.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
 
-//    private  MemberService memberService;
+    private  MemberService memberService;
 
     //Setter Injection
 //    @Autowired
@@ -18,11 +21,26 @@ public class MemberController {
 
     //Constructor Injection
 
-    private final MemberService memberService;
+//    private final MemberService memberService;
 
 
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }
