e03f507b318ad5250ecc934d2641096e1c41e873
skysrd <37787079+skysrd@users.noreply.github.com>
2022-02-16 17:07:36 +0900

    [PATCH] SpringConfig

--- src/main/java/Skysrd/SpringPractice/SpringConfig.java
+++ src/main/java/Skysrd/SpringPractice/SpringConfig.java
@@ -1,24 +1,37 @@
 package Skysrd.SpringPractice;
 
+import Skysrd.SpringPractice.repository.JdbcMemberRepository;
 import Skysrd.SpringPractice.repository.MemberRepository;
 import Skysrd.SpringPractice.repository.MemoryMemberRepository;
 import Skysrd.SpringPractice.service.MemberService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.support.DatabaseStartupValidator;
+
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
 
 @Configuration
 public class SpringConfig {
 
-//    @Bean
-//    public MemberService memberService() {
-//        return new MemberService(memberRepository());
-//    }
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource=dataSource;
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
