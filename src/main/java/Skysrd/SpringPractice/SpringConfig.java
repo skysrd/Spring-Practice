985574a548d1decdf7bc0d24cb52f04be8cf48fa
skysrd <37787079+skysrd@users.noreply.github.com>
2022-03-03 16:00:43 +0900

    [ADD] JpaMemberRepository

--- src/main/java/Skysrd/SpringPractice/SpringConfig.java
+++ src/main/java/Skysrd/SpringPractice/SpringConfig.java
@@ -1,9 +1,6 @@
 package Skysrd.SpringPractice;
 
-import Skysrd.SpringPractice.repository.JdbcMemberRepository;
-import Skysrd.SpringPractice.repository.JdbcTemplateMemberRepository;
-import Skysrd.SpringPractice.repository.MemberRepository;
-import Skysrd.SpringPractice.repository.MemoryMemberRepository;
+import Skysrd.SpringPractice.repository.*;
 import Skysrd.SpringPractice.service.MemberService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,17 +10,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.support.DatabaseStartupValidator;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 import javax.xml.crypto.Data;
 
 @Configuration
 public class SpringConfig {
 
-    private DataSource dataSource;
+    private EntityManager entityManager;
 
     @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource=dataSource;
+    public SpringConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
     }
 
 //    @Bean
@@ -34,6 +32,7 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository() {
 //        return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(entityManager);
     }
 }
